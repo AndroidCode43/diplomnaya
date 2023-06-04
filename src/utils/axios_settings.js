a11f2c6d711e10803faadc9b10f5d27f84a4191e
Dihiko <newdeveloper43@gmail.com>
2023-06-04 18:20:50 +0300

    Update axios_settings.js

--- src/utils/axios_settings.js
+++ src/utils/axios_settings.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const yttAxios = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://yttserver-production.up.railway.app',
     headers: {
         Authorization: `Bearer ${Cookies.get('token')}`
     }
